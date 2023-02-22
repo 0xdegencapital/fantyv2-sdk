0cf8397dbaa3f6fc641b64cd1ef14165bfac644f
zerorekt <brilliantedgar@gmail.com>
2023-02-22 16:52:56 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/constants.ts
@@ -0,0 +1,14 @@
+import JSBI from 'jsbi'
+
+export const FACTORY_ADDRESS = '0x96f8005E358A2B8e4f7b3d74dF5FE6751887c9ED'
+
+export const INIT_CODE_HASH = '0x2c272808f669dbb141e4759d110f215f9bd446cabd2be19f80565277bfd2139c'
+
+export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
+
+// exports for internal consumption
+export const ZERO = JSBI.BigInt(0)
+export const ONE = JSBI.BigInt(1)
+export const FIVE = JSBI.BigInt(5)
+export const _997 = JSBI.BigInt(997)
+export const _1000 = JSBI.BigInt(1000)
